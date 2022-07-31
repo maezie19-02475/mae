b418668d56c69a5b020e2690431d938268e38fae
maezie19-02475 <cmaezielandicho@gmail.com>
2022-07-31 20:31:09 +0800

    Update the code

--- Sample/src/app/app-routing.module.ts
+++ Sample/src/app/app-routing.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  /*{
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+  },*/
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'index',
+    path: '',
     loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
   }
+  /*{
+    path: 'welcome',
+    loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  }*/
 ];
 @NgModule({
   imports: [
--- Sample/src/app/index/index-routing.module.ts
+++ Sample/src/app/index/index-routing.module.ts
@@ -6,7 +6,25 @@ import { IndexPage } from './index.page';
 const routes: Routes = [
   {
     path: '',
-    component: IndexPage
+    component: IndexPage,
+    children:[
+      {
+        path:'',
+        loadChildren:()=>import('../pages/welcome/welcome.module').then(m=>m.WelcomePageModule)
+      },
+      {
+        path:'login',
+        loadChildren:()=>import('../pages/login/login.module').then(m=>m.LoginPageModule)
+      },
+      {
+        path:'signup',
+        loadChildren:()=>import('../pages/signup/signup.module').then(m=>m.SignupPageModule)
+      },
+      {
+        path:'welcome',
+        loadChildren:()=>import('../pages/welcome/welcome.module').then(m=>m.WelcomePageModule)
+      }
+    ]
   }
 ];
 
